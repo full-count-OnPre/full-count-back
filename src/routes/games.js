f5c2f30f19d2dcf5b24716b1016c7ee2f45fe7ac
seungwankim <seungwankim364@gmail.com>
2026-03-11 17:45:02 +0900

    Fix: newer version

--- src/routes/games.js
+++ src/routes/games.js
@@ -1,4 +1,4 @@
-﻿const express = require('express');
+const express = require('express');
 const router = express.Router();
 const gamesController = require('../controllers/gamesController');
 const cache = require('../cache');
@@ -6,8 +6,10 @@ const cache = require('../cache');
 // 경기 목록 조회
 router.get('/', cache(10), gamesController.getGames);
 
-// 경기 상세
+// 경기 상세 (중계 데이터)
 router.get('/:gameId', cache(10), gamesController.getGameById);
+router.get('/', gamesController.getGames);
+router.get('/:gameId', gamesController.getGameById);
 router.get('/:gameId/live', gamesController.getGameLive);
 router.get('/:gameId/relay', gamesController.getGameRelay);
 router.get('/:gameId/chat', gamesController.getGameChat);
